e8ea8f44cdc093f4bf517f111d0fab6b008d3a13
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2022-02-12 19:22:55 +0100

    updated midi names to raspberry default

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     script: 'midico.js',
     env: {
-      "MG30_MIDINAME": "NUX MG-30",
-      "GBOARD_MIDINAME": "iCON G_Boar V1.03",
+      "MG30_MIDINAME": "NUX MG-30:NUX MG-30 MIDI 1 28:",
+      "GBOARD_MIDINAME": "iCON G_Boar V1.03:iCON G_Boar V1.03 MIDI 1 20:0",
     }
   }]
 };
